14cdbb35fea1761659430cff86f21255892fb879
hodish1 <hodaia43@gmail.com>
2021-01-10 01:16:11 +0200

    vue cli

---
+++ src/components/product.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="card">
+    <img :src="product.image" class="card-img-top" :alt="product.title" />
+    <div class="card-body d-flex flex-column justify-content-center align-items-center">
+      <h5 class="card-title">{{ product.title }}</h5>
+      <p class="card-text">
+        <!-- {{ product.description }} -->
+      </p>
+      <p class="card-text">
+        <small class="text-muted">Price {{ product.price }}$</small>
+        <br>
+        <button class="btn btn-primary">Buy Now</button>
+      </p>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "ProductComponent",
+  props: ["product"],
+};
+</script>
+
+<style>
+
+.card-img-top{
+  height: 200px;
+  object-fit: cover;
+}
+
+.card{
+   height: 100%;
+}
+</style>
---
+++ src/components/products.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="container">
+    <div class="row align-items-start">
+      <template v-for="product in products" :key="product.id">
+        <div class="card-wrapper col-12 col-md-6 col-lg-3 mb-3">
+          <ProductComponent :product="product"></ProductComponent>
+        </div>
+      </template>
+    </div>
+  </div>
+</template>
+
+<script>
+import ProductComponent from "./product";
+export default {
+  name: "ProductsComponent",
+  props: ["products"],
+  components: {
+    ProductComponent,
+  },
+};
+</script>
+
+<style>
+.card-wrapper{
+  height: 450px;
+}
+</style>
